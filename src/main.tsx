3241ea26f41f04919b474b4a04c93f2d6af9f929
BonsAi Studio / Digital Architecture <studio@itbonsai.pl>
2026-09-19 15:19:51 +0200

    Make BonsAI Garden workshop the primary website

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { AppOS } from './AppOS';
-import './os-styles.css';
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><AppOS /></React.StrictMode>);
+import { App } from './App';
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
